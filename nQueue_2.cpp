77df56178a136088393be2c61e6b0640b113a411
xujiale <xujl@pandaremit.com>
2025-07-21 23:52:43 +0800

    n皇后的另一种解法

---
+++ nQueue_2.cpp
@@ -0,0 +1,44 @@
+//
+// Created by LoadingV on 2025/7/21.
+//
+
+
+#include<iostream>
+using namespace std;
+
+int n;
+const int N = 10;
+
+bool row[N], col[N], d1[2 * N], d2[2 * N];
+char map[N][N];
+
+void dfs(int x, int y, int s) {
+    if (y == n) x++, y = 0;
+    if (x == n) {
+        if (s == n) {
+            for (int i = 0; i < n; i++) puts(map[i]);
+            puts("");
+        }
+        return;
+    }
+    dfs(x, y + 1, s);
+    if (!row[x] && !col[y] && !d1[n - x + y] && !d2[x + y]) {
+        row[x] = col[y] = d1[n - x + y] = d2[x + y] = true;
+        map[x][y] = 'Q';
+        dfs(x, y + 1, s + 1);
+        row[x] = col[y] = d1[n - x + y] = d2[x + y] = false;
+        map[x][y] = '.';
+    }
+}
+
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            map[i][j] = '.';
+        }
+    }
+    dfs(0, 0, 0);
+    return 0;
+}
